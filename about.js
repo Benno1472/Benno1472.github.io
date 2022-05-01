a9b3f2d31a9bbfa9ca4e4183af89ac49c039a15a
Ben Leszczynski <LeszczynskiBen@gmail.com>
2022-05-01 16:49:18 -0600

    Refactored code
    Combined like code into same file in preparation for multiple pages

--- about.js
+++ about.js
@@ -149,3 +149,176 @@ function computeCurve(p1, p2) {
 
     return (bezierPoints);
 }
+
+class Firework {
+
+    constructor(pos, curvePoints) {
+        this.position = createVector(pos.x, pos.y);
+        this.velocity = createVector(0, 0);
+
+        this.curvePoints = curvePoints
+        this.index = 0;
+
+        this.colour = random(255)
+
+        this.rotation = 0;
+        this.rotationAngle = 0;
+
+        this.computeRotation();
+    }
+
+    methods() {
+        this.update();
+        this.changeRotation()
+        this.display();
+    }
+
+    update() {
+        // physics engine
+
+        // compute acceleration vector between the firework and the current curvepoint
+        this.acceleration = p5.Vector.sub(this.curvePoints[this.index], this.position);
+
+        // normalize the velocity vector length to 1
+        this.velocity = p5.Vector.normalize(this.velocity);
+
+        // add the acceleration vector to the velocity vector
+        this.velocity.add(this.acceleration);
+        // add the velocity vector to the position vector
+        this.position.add(this.velocity);
+
+        // compute the distance between the firework and the current curvepoint
+        let distance = p5.Vector.dist(this.position, this.curvePoints[this.index]);
+
+        // if the distance is close enough, then change the 'target' to the next curve point
+        if (distance <= 10) {
+            this.index += 1;
+        }
+    }
+
+    createStar(x, y, radius1, radius2, npoints) {
+        let angle = TWO_PI / npoints;
+        let halfAngle = angle / 2.0;
+        beginShape();
+        for (let a = 0; a < TWO_PI; a += angle) {
+            let sx = x + cos(a) * radius2;
+            let sy = y + sin(a) * radius2;
+            vertex(sx, sy);
+            sx = x + cos(a + halfAngle) * radius1;
+            sy = y + sin(a + halfAngle) * radius1;
+            vertex(sx, sy);
+        }
+        endShape(CLOSE);
+    }
+
+    display() {
+        fill(this.colour, 255, 255);
+        noStroke();
+
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(this.rotationAngle);
+        this.createStar(0, 0, 5, 13, 5);
+        pop();
+    }
+
+    checkExploded() {
+        if (this.index >= this.curvePoints.length) {
+            return (true);
+        } else if (this.index < this.curvePoints.length) {
+            return (false);
+        }
+    }
+
+    computeRotation() {
+        this.lastPoint = this.curvePoints[this.curvePoints.length - 1];
+
+        if (this.lastPoint.x > width / 2) {
+            this.rotation = 0.1;
+        } else {
+            this.rotation = -0.1;
+        }
+    }
+    changeRotation() {
+        this.rotationAngle += this.rotation
+    }
+
+    getCoords() {
+        return (this.position);
+    }
+
+    getColour() {
+        return (this.colour);
+    }
+}
+
+class Particle {
+
+    constructor(xPos, yPos, rate, force, colour) {
+        this.position = createVector(xPos, yPos);
+        this.acceleration = createVector(0, 0);
+
+        this.velocity = p5.Vector.random2D();
+        this.velocity.mult(force);
+
+        this.life = 255;
+        this.rate = rate;
+        this.colour = colour;
+    }
+
+    methods() {
+        this.update();
+        this.lifespan();
+        this.display();
+    }
+
+    update() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.acceleration.mult(0);
+    }
+
+    lifespan() {
+        this.life -= this.rate;
+    }
+
+    display() {
+        stroke(this.colour, this.life, this.life);
+        strokeWeight(5);
+        point(this.position.x, this.position.y);
+    }
+
+    checkDead() {
+        if (this.life <= 0) {
+            return (true);
+        } else if (this.life > 0) {
+            return (false);
+        }
+    }
+}
+
+class Tile {
+
+    constructor(tileCenter, tileWidth, tileHeight) {
+        this.tileCenter = tileCenter;
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+    }
+
+    methods() {
+        //this.display();
+    }
+
+    display() {
+        stroke(80, 255, 255);
+        noFill();
+        strokeWeight(2);
+        rect(this.tileCenter.x, this.tileCenter.y, this.tileWidth - 5, this.tileHeight - 5);
+        strokeWeight(10);
+        point(this.tileCenter.x, this.tileCenter.y);
+    }
+
+    getCenter() {
+        return (this.tileCenter);
+    }
+}
